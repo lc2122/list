e6c378e0bddc201ae1523459fdfb3a8215f3db0b
lc2122 <76505990+lc2122@users.noreply.github.com>
2024-06-30 17:42:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const videoContainer = document.querySelector('.video-container');
 const m3u8Urls = [
-  'https://api.sooplive.com/media/live/soopbaseball1/master.m3u8
-', 'https://api.sooplive.com/media/live/soopbaseball2/master.m3u8
-', 'https://api.sooplive.com/media/live/soopbaseball3/master.m3u8
-', 'https://api.sooplive.com/media/live/soopbaseball4/master.m3u8
-', 'https://api.sooplive.com/media/live/soopbaseball5/master.m3u8
-' // m3u8 주소 입력
+  'https://api.sooplive.com/media/live/soopbaseball1/master.m3u8', 
+  'https://api.sooplive.com/media/live/soopbaseball2/master.m3u8', 
+  'https://api.sooplive.com/media/live/soopbaseball3/master.m3u8', 
+  'https://api.sooplive.com/media/live/soopbaseball4/master.m3u8', 
+  'https://api.sooplive.com/media/live/soopbaseball5/master.m3u8' // m3u8 주소 입력
 ];
 
 m3u8Urls.forEach(url => {
